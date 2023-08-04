b5cb3e5156304ee290f84dd9ab5dc70690d6072a
Charisse10 <1995510@edu.vaniercollege.qc.ca>
2023-08-04 10:46:05 -0400

    initialized testing

---
+++ tests/unit/CourseItem.spec.js
@@ -0,0 +1,14 @@
+import { shallowMount } from "@vue/test-utils";
+import CourseItem from "@/components/CourseItem.vue";
+
+describe("CourseItem.vue", () => {
+    it("check if course renders the props", () => {
+
+        
+
+
+
+
+
+    });
+});
